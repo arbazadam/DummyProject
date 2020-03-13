d2a1613cf8b698bb82681931acbb35cc7837a74b
arbazadam <42610433+arbazadam@users.noreply.github.com>
2020-03-13 19:15:07 +0400

    project initialization

---
+++ src/main/java/com/stupid/demo/StupidProjectApplication.java
@@ -0,0 +1,13 @@
+package com.stupid.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StupidProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StupidProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/stupid/demo/StupidProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stupid.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StupidProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
